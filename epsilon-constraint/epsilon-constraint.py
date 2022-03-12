a990b672b2ee3771cef5cfaa6cd78207d52c39ef
yana-sidanych <yana.sid150@gmail.com>
2022-03-12 22:54:13 +0200

    change step creation

--- epsilon-constraint/epsilon-constraint.py
+++ epsilon-constraint/epsilon-constraint.py
@@ -1,6 +1,5 @@
 
-from pydoc import describe
-from numpy import min_scalar_type
+import numpy as np
 from pyomo.environ import *
 import matplotlib.pyplot as plt
 from importlib import import_module
@@ -26,23 +25,31 @@ def create_model():
         model.add_component(name, Constraint(expr = constraint(variables)))
 
     model.f1 = Var()
-    model.C_f1 =  Constraint(expr = model.f1 == PROBLEM.F1(variables))
+    model.C_f1 = Constraint(expr = model.f1 == PROBLEM.F1(variables))
     
     model.f2 = Var()
     model.C_f2 = Constraint(expr = model.f2 == PROBLEM.F2(variables))
 
     return model
 
-def calculate_min_max(model, func):
-    def calcualte_objective(func, sense):
-        model.O_func = Objective(expr = func, sense=sense)
+def calculate_min_max(model):
+    def calcualte_objective(func1, func2):
+        model.O_func = Objective(expr = func1, sense=maximize)
         SOLVER.solve(model)
         model.del_component(model.O_func)
-        return value(func)
-    return {
-        "max" : calcualte_objective(func, sense=maximize),
-        "min" : calcualte_objective(func, sense=minimize)
-    }
+        return value(func1), value(func2)
+    
+    values = {"f1" : {}, "f2" : {}}
+    vals = calcualte_objective(model.f1, model.f2)
+    values["f1"]["max"] = vals[0]
+    values["f2"]["min"] = vals[1]
+
+    vals = calcualte_objective(model.f2, model.f1)
+    values["f2"]["max"] = vals[0]
+    values["f1"]["min"] = vals[1]
+
+    return values
+
 
 def calcualte_pareto_front(model, maximize_func, constraint_func, num_epsilon, min_max):
     pareto_front = {
@@ -54,14 +61,16 @@ def calcualte_pareto_front(model, maximize_func, constraint_func, num_epsilon, m
     model.O_pareto = Objective(expr = model.component(maximize_func) + model.delta * model.slack, sense=maximize)
     model.C_epsilon = Constraint(expr = model.component(constraint_func) - model.slack == model.e)
 
-    step = (min_max[constraint_func]["max"] - min_max[constraint_func]["min"]) / num_epsilon
-    i = min_max[constraint_func]["min"]
-    while i <= min_max[constraint_func]["max"]:
-        model.e = i
+    f_min = min_max[constraint_func]["min"]
+    f_max = min_max[constraint_func]["max"]
+    step = (f_max - f_min) / num_epsilon
+    steps = list(np.arange(f_min, f_max, step)) + [f_max]
+
+    for step in steps:
+        model.e = step
         SOLVER.solve(model)
         pareto_front["f1"].append(value(model.f1))
         pareto_front["f2"].append(value(model.f2))
-        i += step
 
     model.del_component(model.O_pareto)
     model.del_component(model.C_epsilon)
@@ -76,11 +85,9 @@ def update_plot(ax, func, pareto):
 
 def create_pareto_front():
     model = create_model()
-    min_max  = {
-        "f1" : calculate_min_max(model, model.f1),
-        "f2" : calculate_min_max(model, model.f2)
-    }
-
+    min_max  = calculate_min_max(model)
+    
+    print(min_max)
     model.e = Param(initialize=0, mutable=True)
     model.delta = Param(initialize=0.00001)
     model.slack = Var(within=NonNegativeReals)
@@ -88,7 +95,7 @@ def create_pareto_front():
 
     pareto_front_by_f1 = calcualte_pareto_front(model, "f1", "f2", num_iterations, min_max)
     pareto_front_by_f2 = calcualte_pareto_front(model, "f2", "f1", num_iterations, min_max)
-
+    print(pareto_front_by_f1)
 
     f1_min = min(min(pareto_front_by_f1["f1"]), min(pareto_front_by_f2["f1"]))
     f2_min = min(min(pareto_front_by_f1["f2"]), min(pareto_front_by_f2["f2"]))
